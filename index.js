14364d99021fd1f512364ab70ecf48c131377781
CHEASENGHOUR <cheasenghour789@gmail.com>
2025-10-27 15:00:15 +0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const middlewares = jsonserver.defaults();
 const port = process.env.PORT || 8000;
 
 // Add static file serving for images
-server.use('/images', jsonserver.static('./images'));
+// server.use('/images', jsonserver.static('./images'));
 
 server.use(cors());
 server.use(middlewares);
